6f7087e5c51e8db6107d627319f9681312f8e7b7
jorge.castro <nikey_es@yahoo.es>
2014-10-12 23:55:48 +0200

    Mapper

--- MarvelKataLibrary/MarvelKataLibrary/ComicsService.cs
+++ MarvelKataLibrary/MarvelKataLibrary/ComicsService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -15,7 +16,32 @@ namespace MarvelKataLibrary
     public class ComicsService
     {
 
-        private static readonly String _timeStamp = DateTime.Now.ToString();
+        private static readonly String _timeStamp = DateTime.Now.Millisecond.ToString();
+        private static readonly String _publicKey = "97f295907072a970c5df30d73d1f3816";
+        private static readonly String _privateKey = "ed54a875c0dffad1fa6af84e66ff104434a1c6cc";
+        private readonly String _hashApiMarvel;
+
+        public ComicsService()
+        {
+            _hashApiMarvel = GetMD5HashData(_timeStamp + _privateKey + _publicKey);
+        }
+
+        private string GetMD5HashData(string data)
+        {
+            MD5 md5 = MD5.Create();
+
+            byte[] hashData = md5.ComputeHash(Encoding.Default.GetBytes(data));
+
+            StringBuilder returnValue = new StringBuilder();
+
+            for (int i = 0; i < hashData.Length; i++)
+            {
+                returnValue.Append(hashData[i].ToString("X2"));
+            }
+
+            return returnValue.ToString().ToLower();
+
+        }
         public Comic GetComic(String title)
         {
             Comic comic = new Comic();
@@ -35,27 +61,18 @@ namespace MarvelKataLibrary
 
         public List<Comic> GetComicsNextWeek()
         {
-            List<Comic> comicList = new List<Comic>();
+            List<Comic> comicList;
             using (WebClient proxy = new WebClient())
             {
-                String urlProxyMarvel = String.Format("http://gateway.marvel.com:80/v1/public/comics?{0}&{1}", "dateDescriptor=nextWeek", "ts=987&apikey=97f295907072a970c5df30d73d1f3816&hash=abfa1c1d42a73a5eab042242335d805d");
+
+                String urlProxyMarvel = String.Format("http://gateway.marvel.com:80/v1/public/comics?{0}&ts={1}&apikey={2}&hash={3}", "dateDescriptor=nextWeek", _timeStamp, _publicKey, _hashApiMarvel);
                 String response = proxy.DownloadString(urlProxyMarvel);
                 JObject results = JObject.Parse(response);
 
                 JArray resultComics = (JArray)results["data"]["results"];
+                MarvelAPIMapper mapper = new MarvelAPIMapper();
 
-                Comic comic;
-                foreach (JObject resultComic in resultComics)
-                {
-                    comic = new Comic();
-
-                    comic.Title = (String)resultComic["title"];
-                    JToken thumbail = resultComic["thumbnail"];
-                    comic.ThumbailUrl = thumbail["path"] + "." + thumbail["extension"];
-                    comic.Price = double.Parse(resultComic["prices"][0]["price"].ToString());
-
-                    comicList.Add(comic);
-                }
+                comicList = mapper.GetComicFromMarvelAPIData(resultComics);
                 
             }
             return comicList;
---
+++ MarvelKataLibrary/MarvelKataLibrary/MarvelAPIMapper.cs
@@ -0,0 +1,32 @@
+﻿using MarvelKataLibrary.Entities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelKataLibrary
+{
+    public class MarvelAPIMapper
+    {
+            public List<Comic> GetComicFromMarvelAPIData(JArray arrayMarvelJSON)
+            {
+                List<Comic> comicList = new List<Comic>();
+                Comic comic;
+                foreach (JObject resultComic in arrayMarvelJSON)
+                {
+                    comic = new Comic();
+
+                    comic.Title = (String)resultComic["title"];
+                    JToken thumbail = resultComic["thumbnail"];
+                    comic.ThumbailUrl = thumbail["path"] + "." + thumbail["extension"];
+                    comic.Price = double.Parse(resultComic["prices"][0]["price"].ToString());
+
+                    comicList.Add(comic);
+                }
+
+                return comicList;
+            }
+    }
+}
