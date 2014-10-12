d3734e2f4587c72917399cd81792879835f69138
jorge.castro <nikey_es@yahoo.es>
2014-10-12 23:00:31 +0200

    Primeras Pruebas

--- MarvelKataLibrary/MarvelKataLibrary.UnitTest/ComicServiceTests.cs
+++ MarvelKataLibrary/MarvelKataLibrary.UnitTest/ComicServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MarvelKataLibrary.Entities;
+using System.Collections.Generic;
 
 namespace MarvelKataLibrary.UnitTest
 {
@@ -27,6 +28,30 @@ namespace MarvelKataLibrary.UnitTest
             Assert.AreEqual(expected.Title, actual.Title, "Error Name");
             Assert.AreEqual(expected.ThumbailUrl, actual.ThumbailUrl,  "Error ThumbailUrl");
             Assert.AreEqual(expected.Price, actual.Price, "Error Price");
+
+        }
+
+
+        [TestMethod]
+        public void GetComicsbyCharacterIdTest()
+        {
+            //ARRANGE
+            ComicsService sut = new ComicsService();
+            /*Comic expected = new Comic()
+            {
+                Title = "Ultimate Spider-Man 200 (2011) #200 (Marquez Variant)",
+                ThumbailUrl = "http://i.annihil.us/u/prod/marvel/i/mg/7/00/5339aada49977.jpg",
+                Price = 4.99
+            };*/
+
+            //ACT
+            List<Comic> actual = sut.GetComicsNextWeek();
+            
+            //ASSERT
+
+            /*Assert.AreEqual(expected.Title, actual.Title, "Error Name");
+            Assert.AreEqual(expected.ThumbailUrl, actual.ThumbailUrl, "Error ThumbailUrl");
+            Assert.AreEqual(expected.Price, actual.Price, "Error Price");*/
 
         }
     }
--- MarvelKataLibrary/MarvelKataLibrary/ComicsService.cs
+++ MarvelKataLibrary/MarvelKataLibrary/ComicsService.cs
@@ -30,5 +30,33 @@ namespace MarvelKataLibrary
             } 
             return comic;
         }
+
+        public List<Comic> GetComicsNextWeek()
+        {
+            List<Comic> comicList = new List<Comic>();
+            using (WebClient proxy = new WebClient())
+            {
+                String urlProxyMarvel = String.Format("http://gateway.marvel.com:80/v1/public/comics?{0}&{1}", "dateDescriptor=nextWeek", "ts=987&apikey=97f295907072a970c5df30d73d1f3816&hash=abfa1c1d42a73a5eab042242335d805d");
+                String response = proxy.DownloadString(urlProxyMarvel);
+                JObject results = JObject.Parse(response);
+
+                JArray resultComics = (JArray)results["data"]["results"];
+
+                Comic comic;
+                foreach (JObject resultComic in resultComics)
+                {
+                    comic = new Comic();
+
+                    comic.Title = (String)resultComic["title"];
+                    JToken thumbail = resultComic["thumbnail"];
+                    comic.ThumbailUrl = thumbail["path"] + "." + thumbail["extension"];
+                    comic.Price = double.Parse(resultComic["prices"][0]["price"].ToString());
+
+                    comicList.Add(comic);
+                }
+                
+            }
+            return comicList;
+        }
     }
 }
